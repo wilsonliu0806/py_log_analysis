75d3c208b616573fb3a7115a282f36adbb958cd5
wilsonliu0806@gmail.com <wilsonliu0806@gmail.com>
2016-08-30 15:47:39 +0800

    add config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+'''
+config file
+'''
+import datetime
+path_ssd_move_log = 'E:\\python_file\\ssd_move.log'
+path_access_log = 'E:\\python_file\\accesslog'
+#move_out start and end date
+start_date = datetime.datetime(2016,7,24)
+end_date = datetime.datetime(2016,7,25)
+
